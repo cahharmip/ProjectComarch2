2bbc714fe16453e9bd39695ce7b28bab4b55a219
ckhoarmnp <sorapol57@gmail.com>
2015-05-09 14:24:29 +0700

    test

---
+++ register.v
@@ -0,0 +1,17 @@
+/*
+file : Register
+By   : Sorapol  Sroysuwan 5610503990
+	   Jackthip Phureesatian 5610500966
+Date : 9/5/2558
+*/
+
+module register (
+	input clk,    // Clock
+	input clk_en, // Clock Enable
+	input rst_n,  // Asynchronous reset active low
+	
+);
+
+
+
+endmodule
